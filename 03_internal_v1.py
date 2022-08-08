f8b6834600c0442fa9ae68962c19fce5c068d1f8
Jessica Allen <101675276+jessicaall@users.noreply.github.com>
2022-08-08 20:02:32 +1200

    Calculator Versions / Trials Updated

--- 03_internal_v1.py
+++ 03_internal_v1.py
@@ -1,123 +1,76 @@
+# calculator V1
 from tkinter import *
 from functools import partial # To prevent unwanted windows
 import random
-import re
+import re        
 
-class View:
+class Calculations:
     def __init__(self, partner):
-        
-##        # disable button
-##        partner.enter.config(state=DISABLED)
 
-        # Sets up child window (ie. help box)
-        self.view_box = Toplevel()
+        travel_charge = []
+        virus_charge = []
+        wof_and_tune_charge = []
 
-        # If users press cross at top, closes help and 'releases help button
-        # self.view_box.protocol('WM_DELETE_WINDOW', partial(self.close_view, partner))
-
-
-        
-        # GUI 
-        self.view_frame = Frame(self.view_box, padx=10, pady=10)
-        self.view_frame.grid()
-
+        #constant as the fixed rate could change
+        SET_CHARGE = 10
+        FIXED_DISTANCE = 5
+        WOF_COST = 100
+        VIRUS_PROTECTION_COST = 0.8
+        CENTS_PER_EXTRA_KM = 0.5
         
 
-        # Image logo (row 0)
-        photo = PhotoImage(file="logo3.png")
-        self.logo_label = Label(self.view_frame,
-                                image=photo, padx=10, pady=10,
-                                justify=CENTER)
-        self.logo_label.photo = photo
-        self.logo_label.grid(row=0)
+        # Travel charge
+        for i in dist_travelled:
+            round(i)
+            if i <= FIXED_DISTANCE and i > 0:
+                travel_charge.append(SET_CHARGE)
 
-        # Message (row 1)
-        self.message = Label(self.view_frame, text="All Jobs",
-                                     font="Arial 25 bold ",
-                                     justify=CENTER,
-                                     padx=40, pady=40,
-                                     wrap=275)
-        self.message.grid(row=1)
+            elif i > FIXED_DISTANCE:
+                travel_cost = SET_CHARGE + (i-FIXED_DISTANCE) * CENTS_PER_EXTRA_KM
+                travel_charge.append(travel_cost)
 
+            elif i == 0:
+                travel_cost = 0
+                travel_charge.append(travel_cost)
 
-        # Info
-        self.info_frame = Frame(self.view_frame, width=200)
-        self.info_frame.grid()
+        # Virus protection charge
+        for v in virus_protection_mins:
+            round(v)
+            virus_cost = v * VIRUS_PROTECTION_COST
+            virus_charge.append(virus_cost)
 
-        stop = "n"
-        num = 0
+        # wof and tune charge
+        for w in wof_tune:
+            if w == 'yes':
+                wof_and_tune_charge.append(WOF_COST)
+                
+            else:
+                wof_and_tune_charge.append(0)
 
-        while stop != "y":
+        number = 0
+        for item in travel_charge:
+            total_cost = item + virus_charge[number] + wof_and_tune_charge[number]
+            job_charge.append(total_cost)
+            number += 1
 
-            # job number
-            self.job_label = Label(text="Job Number: ")
-            self.job_label.grid(row=0, column=0)
-
-            self.job_no_label = Label(self.info_frame, text=job_number[num])
-            self.job_no_label.grid(row=0, column=1)
-
-            # customer name
-            self.cust_label = Label(text="Customer Name: ")
-            self.cust_label.grid(row=1, column=0)
-
-            self.cust_name_label = Label(self.info_frame, text=customer_name[num])
-            self.cust_name_label.grid(row=1, column=1)
-
-            # Job Charge
-            self.charge_label = Label(text="Job Charge: ")
-            self.charge_label.grid(row=2, column=0)
-
-            self.charge_name_label = Label(self.info_frame, text=job_charge[num])
-            self.charge_name_label.grid(row=2, column=1)
-
-            # Previous and Next buttons
-            self.back_button = Button(self.info_frame, text="Previous", command=self.to_change(-1, num))
-            self.back_button.grid(row=3, column=0)
-
-            self.next_button = Button(self.info_frame, text="Next", command= self.to_change(1, num))
-            self.next_button.grid(row=3, column=1)
+        print(job_charge)
 
 
-            # Dismiss button
-            self.dismiss_button = Button(self.info_frame, text="Dismiss", command=self.stopper(stop))
-            self.dismiss_button.grid(row=4, column=0)
-
-            # Export Button
-            self.export_button = Button(self.info_frame, text="Export", command=self.stopper(stop))
-            self.export_button.grid(row=4, column=1)
-
-            
-        
-
-    def to_change(self, number, num):
-        num = num + number
-
-        return num
-
-    def stopper(self, stop):
-        stop = "y"
-
-        return stop
-
-##    def close_view(self, partner):
-##        # Put button back to normal...
-##        partner.view_jobs.config(state=NORMAL)
-##        self.view_box.destroy()
 
 
 # main routine
 if __name__ == "__main__":
 
     #Global lists
-    job_number = [1, 2, 3]
-    customer_name = ['jess', 'lana', 'bob']
-    dist_travelled = [2.5, 5, 3]
-    virus_protection_mins = [20, 10, 0]
-    wof_tune = ['no', 'yes', 'yes']
-    job_charge = [26.0, 118.0, 110.0]
+    job_number = [2]
+    customer_name = ['Lana']
+    dist_travelled = [30]
+    virus_protection_mins = [0]
+    wof_tune = ['yes']
+    job_charge = []
     
     root = Tk()
     root.title("Suzy's Mobile Service")
-    something = View(root)
+    something = Calculations(root)
     root.mainloop()
 
--- 03_internal_v2.py
+++ 03_internal_v2.py
@@ -1,108 +1,48 @@
+# calculator V2
 from tkinter import *
 from functools import partial # To prevent unwanted windows
 import random
-import re
+import re        
 
-class View:
-    def __init__(self, partner):
-        
-##        # disable button
-##        partner.enter.config(state=DISABLED)
+    def job_charge(self, self.distance, self.time, self.wof):
 
-        # Sets up child window (ie. help box)
-        self.view_box = Toplevel()
+        #constant as the fixed rate could change
+        SET_CHARGE = 10
+        FIXED_DISTANCE = 5
+        WOF_COST = 100
+        VIRUS_PROTECTION_COST = 0.8
 
-        # If users press cross at top, closes help and 'releases help button
-        # self.view_box.protocol('WM_DELETE_WINDOW', partial(self.close_view, partner))
-
-
-        
-        # GUI 
-        self.view_frame = Frame(self.view_box, padx=10, pady=10)
-        self.view_frame.grid()
-
-        
-
-        # Image logo (row 0)
-        photo = PhotoImage(file="logo3.png")
-        self.logo_label = Label(self.view_frame,
-                                image=photo, padx=10, pady=10,
-                                justify=CENTER)
-        self.logo_label.photo = photo
-        self.logo_label.grid(row=0)
-
-        # Message (row 1)
-        self.message = Label(self.view_frame, text="All Jobs",
-                                     font="Arial 25 bold ",
-                                     justify=CENTER,
-                                     padx=40, pady=40,
-                                     wrap=275)
-        self.message.grid(row=1)
-
-
-        # Info
-        self.info_frame = Frame(self.view_frame, width=200)
-        self.info_frame.grid()
-
-        stop = "n"
-        num = 0
-
-        while stop != "y":
-
-            # job number
-            self.job_label = Label(text="Job Number: ")
-            self.job_label.grid(row=0, column=0)
-
-            self.job_no_label = Label(self.info_frame, text=job_number[num])
-            self.job_no_label.grid(row=0, column=1)
-
-            # customer name
-            self.cust_label = Label(text="Customer Name: ")
-            self.cust_label.grid(row=1, column=0)
-
-            self.cust_name_label = Label(self.info_frame, text=customer_name[num])
-            self.cust_name_label.grid(row=1, column=1)
-
-            # Job Charge
-            self.charge_label = Label(text="Job Charge: ")
-            self.charge_label.grid(row=2, column=0)
-
-            self.charge_name_label = Label(self.info_frame, text=job_charge[num])
-            self.charge_name_label.grid(row=2, column=1)
-
-            # Previous and Next buttons
-            self.back_button = Button(self.info_frame, text="Previous", command=self.to_change(-1, num))
-            self.back_button.grid(row=3, column=0)
-
-            self.next_button = Button(self.info_frame, text="Next", command= self.to_change(1, num))
-            self.next_button.grid(row=3, column=1)
-
-
-            # Dismiss button
-            self.dismiss_button = Button(self.info_frame, text="Dismiss", command=self.stopper(stop))
-            self.dismiss_button.grid(row=4, column=0)
-
-            # Export Button
-            self.export_button = Button(self.info_frame, text="Export", command=self.stopper(stop))
-            self.export_button.grid(row=4, column=1)
-
+        #rounding up
+        if self.distance % 1 >= 0.5:
+            distance_rounded = round(self.distance)
             
+        #rounding down
+        elif self.distance % 1 < 0.5:
+            distance_rounded = round(self.distance)
         
+        #if distance is greater than 5km
+        if distance_rounded > FIXED_DISTANCE:
+            add_on = distance_rounded - 5
+            add_on *= 0.5
+            SET_CHARGE += add_on
 
-    def to_change(self, number, num):
-        num = num + number
+        #time for virus protection
+        if self.time != 0:
+            SET_CHARGE += self.time * VIRUS_PROTECTION_COST
 
-        return num
+        #using wof
+        if self.wof != "NO":
+            SET_CHARGE += WOF_COST
 
-    def stopper(self, stop):
-        stop = "y"
+        #formatted job charge to 2 deciaml places for currency
+        job_charge_formatted = format(SET_CHARGE, ".2f")
+        
+        return job_charge_formatted
 
-        return stop
 
-##    def close_view(self, partner):
-##        # Put button back to normal...
-##        partner.view_jobs.config(state=NORMAL)
-##        self.view_box.destroy()
+
+
+
 
 
 # main routine
@@ -111,14 +51,13 @@ if __name__ == "__main__":
     #Global lists
     job_number = [1, 2, 3]
     customer_name = ['jess', 'lana', 'bob']
-    dist_travelled = [2.5, 5, 3]
+    dist_travelled = [6, 5, 3.5]
     virus_protection_mins = [20, 10, 0]
     wof_tune = ['no', 'yes', 'yes']
-    job_charge = [26.0, 118.0, 110.0]
+    job_charge = []
     
     root = Tk()
     root.title("Suzy's Mobile Service")
-    something = View(root)
+    something = Calculations(root)
     root.mainloop()
-
 
